362e7d6b38ead7a2eb2a904d48580d60303cb71c
AdrianSim2001 <101225244@students.swinburne.edu.my>
2022-07-07 14:58:17 +1000

    fixed adminLogout.php

--- Project/adminLogout.php
+++ Project/adminLogout.php
@@ -2,9 +2,9 @@
 session_start();
 if (isset($_SESSION['admin'])) {
     unset($_SESSION['admin']);
-    if(isset($_SESSION['convenor'])){
-        unset($_SESSION['convenor']);
-    }
+}
+if(isset($_SESSION['convenor'])){
+    unset($_SESSION['convenor']);
 }
 session_unset();
 session_destroy(); //Destroy session
