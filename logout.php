c537663f3149b2de2859bbc2148c32d958255408
EthanChongXZ <103698851@student.swin.edu.au>
2022-07-10 22:46:17 +1000

    Removed old files

--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['user']);	//Remove session variables
-session_destroy();			//Destroy session
-header("Location: studentlogin.html");	//Brings user back to home page
-?>
