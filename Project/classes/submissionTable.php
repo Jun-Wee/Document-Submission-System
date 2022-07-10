225ef43c77a05376901816881ca8cd2087385df6
AdrianSim2001 <101225244@students.swinburne.edu.my>
2022-07-11 02:30:08 +1000

    added control to convenor to allow viewing only submissions that are related to own teaching units

--- Project/classes/submissionTable.php
+++ Project/classes/submissionTable.php
@@ -10,44 +10,66 @@ class SubmissionTable
         $this->db = $db;
     }
 
-    function GetAll($convenorId = 0)
+    function GetAll($teachingUnits = null)
     {
         // retrieve all submission records from database using database object -->
 
         // Create connection
         $this->db->createConnection();
 
-        if ($convenorId == 0) {
+        if ($teachingUnits == null) {
             $sql = "SELECT * FROM submission";
-        } else {
-            $sql = "SELECT * FROM submission WHERE convenorID = ?";
-        }
-        $prepared_stmt = mysqli_prepare($this->db->getConnection(), $sql);
 
-        if ($convenorId != 0) {
-            //Bind input variables to prepared statement
-            $prepared_stmt->bind_param("s", $convenorId);
-        }
+            $prepared_stmt = mysqli_prepare($this->db->getConnection(), $sql);
 
-        //Execute prepared statement
-        mysqli_stmt_execute($prepared_stmt);
+            //Execute prepared statement
+            mysqli_stmt_execute($prepared_stmt);
 
-        // Get resultset
-        $queryResult =  mysqli_stmt_get_result($prepared_stmt)
-            or die("<p>Unable to select from database table</p>");
+            // Get resultset
+            $queryResult =  mysqli_stmt_get_result($prepared_stmt)
+                or die("<p>Unable to select from database table</p>");
 
-        // Close the prepared statement
-        @mysqli_stmt_close($prepared_stmt);
+            // Close the prepared statement
+            @mysqli_stmt_close($prepared_stmt);
 
-        $row = mysqli_fetch_row($queryResult);
-
-        $item_count = 0;
-        while ($row) {
-
-            // fetch the record from the server and then store them in an object
-            $this->submissions[$item_count] = new Submission($row[0], $row[1], $row[2], $row[3], $row[4], $row[5]);
             $row = mysqli_fetch_row($queryResult);
-            $item_count += 1;
+
+            $item_count = 0;
+            while ($row) {
+                // fetch the record from the server and then store them in an object
+                $this->submissions[$item_count] = new Submission($row[0], $row[1], $row[2], $row[3], $row[4], $row[5]);
+                $row = mysqli_fetch_row($queryResult);
+                $item_count += 1;
+            }
+        } else {
+            for ($i = 0; $i < count($teachingUnits); $i++) {
+                $sql = "SELECT * FROM `submission` WHERE `unitCode` = ?";
+
+                $prepared_stmt = mysqli_prepare($this->db->getConnection(), $sql);
+
+                //Bind input variables to prepared statement
+                $prepared_stmt->bind_param("s", $teachingUnits[$i]["code"]);
+
+                //Execute prepared statement
+                mysqli_stmt_execute($prepared_stmt);
+
+                // Get resultset
+                $queryResult =  mysqli_stmt_get_result($prepared_stmt)
+                    or die("<p>Unable to select from database table</p>");
+
+                // Close the prepared statement
+                @mysqli_stmt_close($prepared_stmt);
+
+                $row = mysqli_fetch_row($queryResult);
+
+                $item_count = 0;
+                while ($row) {
+                    // fetch the record from the server and then store them in an object
+                    array_push($this->submissions, new Submission($row[0], $row[1], $row[2], $row[3], $row[4], $row[5]));
+                    $row = mysqli_fetch_row($queryResult);
+                    $item_count += 1;
+                }
+            }
         }
 
         $this->db->closeConnection();
