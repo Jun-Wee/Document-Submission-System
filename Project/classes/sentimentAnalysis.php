d15d27ae61e7815bbe7a6027f2539bcd7619961b
EthanChongXZ <103698851@student.swin.edu.au>
2022-07-12 16:39:31 +1000

    Removed sentimentAnalysis function and added database call function

--- Project/classes/sentimentAnalysis.php
+++ Project/classes/sentimentAnalysis.php
@@ -5,56 +5,82 @@ class SentimentAnalysis {
     private $score;
     private $magnitude;
 
-    function sentimentAnalysis() {                  //Fill this spot in the future
-
-    }
-
     function evaluate($annotationS) {               //Fill this spot with return values
         //Determine the sentiment value from score and magnitude
         //Score = Positive/Negative statement
         //Magnitude = Severity of emotion put into the sentence
-        if ($annotationS->sentiment()['score'] > 0) {                   //Positive messages----------------------------
-            if ($annotationS->sentiment()['magnitude'] >= 3) {
-                echo "This is an extremely positive message!";
+
+        $score = $annotationS->sentiment()['score'];
+        $magnitude = $annotationS->sentiment()['magnitude'];
+
+        if ($score > 0) {                   //Positive messages----------------------------
+            if ($magnitude >= 3) {
+                $emotion = "This is an extremely positive message!";
+                echo $emotion;                              //To be deleted at final stage
+                return $emotion;
             }
 
-            if ($annotationS->sentiment()['magnitude'] >= 1.5 && $annotationS->sentiment()['magnitude'] < 3) {
-                echo "This is a very positive message.";
+            if ($magnitude >= 1.5 && $magnitude < 3) {
+                $emotion = "This is a very positive message.";
+                echo $emotion;                              //To be deleted at final stage
+                return $emotion;
             }
 
-            if ($annotationS->sentiment()['magnitude'] < 1.5) {
-                echo "This is a somewhat positive message.";
-            }
-        }
-
-        if ($annotationS->sentiment()['score'] < 0) {
-            if ($annotationS->sentiment()['magnitude'] >= 3) {          //Negative messages---------------------------
-                echo "This is an extremely negative message!";
-            }
-
-            if ($annotationS->sentiment()['magnitude'] >= 1.5 && $annotationS->sentiment()['magnitude'] < 3) {
-                echo "This is a very negative message.";
-            }
-
-            if ($annotationS->sentiment()['magnitude'] < 1.5) {
-                echo "This is a somewhat negative message.";
+            if ($magnitude< 1.5) {
+                $emotion = "This is a somewhat positive message.";
+                echo $emotion;                              //To be deleted at final stage
+                return $emotion;
             }
         }
 
-        if ($annotationS->sentiment()['score'] == 0) {                  //Neutral messages--------------------------
-            if ($annotationS->sentiment()['magnitude'] >= 3) {
-                echo "This message contains extremely mixed emotions!";
+        if ($score < 0) {
+            if ($magnitude >= 3) {          //Negative messages---------------------------
+                $emotion =  "This is an extremely negative message!";
+                echo $emotion;                              //To be deleted at final stage
+                return $emotion;
             }
 
-            if ($annotationS->sentiment()['magnitude'] >= 1.5 && $annotationS->sentiment()['magnitude'] < 3) {
-                echo "This message is very mixed.";
+            if ($magnitude >= 1.5 && $magnitude < 3) {
+                $emotion = "This is a very negative message.";
+                echo $emotion;                              //To be deleted at final stage
+                return $emotion;
             }
 
-            if ($annotationS->sentiment()['magnitude'] < 1.5) {
-                echo "This is a somewhat neutral message.";
+            if ($magnitude < 1.5) {
+                $emotion = "This is a somewhat negative message.";
+                echo $emotion;                              //To be deleted at final stage
+                return $emotion;
+            }
+        }
+
+        if ($score == 0) {                  //Neutral messages--------------------------
+            if ($magnitude >= 3) {
+                $emotion = "This message contains extremely mixed emotions!";
+                echo $emotion;                              //To be deleted at final stage
+                return $emotion;
+            }
+
+            if ($magnitude >= 1.5 && $magnitude < 3) {
+                $emotion = "This message is very mixed.";
+                echo $emotion;                              //To be deleted at final stage
+                return $emotion;
+            }
+
+            if ($magnitude < 1.5) {
+                $emotion = "This is a somewhat neutral message.";
+                echo $emotion;                              //To be deleted at final stage
+                return $emotion;
             }
         }
     }
+
+    //Function to call the analysis table interface to store new analysis records
+    function storeSentiment($db) {
+        //$storeSentiment = new AnalysisTable();
+        //$storeSentiment->add();
+
+    }
+    
 }
 
 ?>
