d858bff2c01f39ef91a2458cbc30505e9f95d028
AdrianSim2001 <101225244@students.swinburne.edu.my>
2022-07-13 20:37:36 +1000

    added delete function to SubmissionTable class

--- Project/classes/submissionTable.php
+++ Project/classes/submissionTable.php
@@ -115,5 +115,24 @@ class SubmissionTable
 
     function Delete($subId)
     {
+        // Create connection
+        $this->db->createConnection();
+
+        $sql = "DELETE FROM `submission` WHERE `Id` = ?";
+
+        $prepared_stmt = mysqli_prepare($this->db->getConnection(), $sql);
+
+        //Bind input variables to prepared statement
+        $prepared_stmt->bind_param("s", $subId);
+
+        //Execute prepared statement
+        $queryResult = mysqli_stmt_execute($prepared_stmt);
+
+        // Close the prepared statement
+        @mysqli_stmt_close($prepared_stmt);
+
+        $this->db->closeConnection();
+
+        return $queryResult;
     }
 }
