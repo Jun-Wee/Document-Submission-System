fe308060100e9dad8e7525351984e12a2757625c
AdrianSim2001 <101225244@students.swinburne.edu.my>
2022-07-13 20:39:33 +1000

    added function alert()

--- Project/system_functions.php
+++ Project/system_functions.php
@@ -226,4 +226,14 @@ function checkUploadedFile($file, $filename, $fileTmpName, $fileError, $fileSize
 
     return [$fileUploadErrorMsg, $fileDestination];
 }
+
+// PHP program to pop an alert
+// message box on the screen
+
+// Function definition
+function function_alert($message)
+{
+    // Display the alert box 
+    echo "<script>alert('$message');</script>";
+}
 ?>
