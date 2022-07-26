56d390bcf7931c4e43e5c378f19e381ab7661ae9
EthanChongXZ <103698851@student.swin.edu.au>
2022-07-26 16:50:56 +1000

    Generated table for entity and alignment fix

--- Project/FPDF/generatePDF.php
+++ Project/FPDF/generatePDF.php
@@ -33,20 +33,21 @@ $pdf->Image($image1, 10, 8, 33);
 $pdf->SetFont('Arial', 'B', '18');
 
 //Add page heading
-$pdf->SetXY(50, 20);
+$pdf->SetXY(55, 20);
 $pdf->Cell(100, 10, 'Analysis Report', 1, 0, 'C', 0);
 
 //Add content-----------------------------------Change this last
 $pdf->SetXY(10, 50);
 $pdf->SetFontSize(12);                          //Change font size
 
-//Document general information
+//Document general information------------------------------------------------------------------------------
 $submission = $submissionTable->getAll();       //Obtain submission details
+//$submission = $submissiontable->get($subId);  //Obtain submission details
 $pdf->Cell(0, 10, 'Student ID: ' . ($submission[0]->getstuId()), 0, 1);
 $pdf->Cell(0, 10, 'Course Code: ' . ($submission[0]->getUnitCode()), 0, 1);
 $pdf->Cell(0, 10, 'Document: ' . ($submission[0]->getFilePath()), 0, 1);
 
-//Document analysis information
+//Document analysis information-----------------------------------------------------------------------------
 $subId = $submission[0]->getId();                  //Obtain submission ID
 $analysisOutput = $analysisTable->getAll();     //Obtain analysis details
 //analysisOutput = $analysisTable->get($subId)  //Obtain analysis details
@@ -56,18 +57,37 @@ $pdf->Cell(0, 10, 'Sentiment magnitude: ' . ($analysisOutput[0]->getSentimentMag
 
 $entityOutput = $entityTable->getAll();         //Obtain entity details
 //$entityOutput = $entityTable->get($subId)     //Obtain entity details     //Make it a table
-for ($i = 0; $i < count($entityOutput); $i++) {
-    $pdf->Cell(0, 10, 'Entity: ' . ($entityOutput[$i]->getName()), 0, 1);
-    $pdf->Cell(0, 10, 'Salience: ' . ($entityOutput[$i]->getSalience()), 0, 1);
-    $pdf->Cell(0, 10, 'Source: ' . ($entityOutput[$i]->getLink()), 0, 1);
+// for ($i = 0; $i < count($entityOutput); $i++) {
+//     $pdf->Cell(0, 10, 'Entity: ' . ($entityOutput[$i]->getName()), 0, 1);
+//     $pdf->Cell(0, 10, 'Salience: ' . ($entityOutput[$i]->getSalience()), 0, 1);
+//     $pdf->Cell(0, 10, 'Source: ' . ($entityOutput[$i]->getLink()), 0, 1);
+// }
+
+//Create a PDF table for entity analysis
+$width_cell = array(20, 60, 40, 70);                                        //Array for column size
+$pdf->SetFont('Arial', 'B', 13);
+
+$pdf->SetFillColor(193, 229, 252);                                          //Set background colour
+
+$pdf->Cell($width_cell[0], 10, 'ID', 1, 0, 'C', true);                      //Set header 
+$pdf->Cell($width_cell[1], 10, 'Entity name', 1, 0, 'C', true);
+$pdf->Cell($width_cell[2], 10, 'Salience score', 1, 0, 'C', true);
+$pdf->Cell($width_cell[3], 10, 'Wikipedia link', 1, 1, 'C', true);
+
+$pdf->SetFont('Arial', '', 12);
+$pdf->SetFillColor(235, 236, 236);                                          //Header background colour;
+$fill = false;
+
+for ($i = 0; $i < count($entityOutput); $i++) {                             //Each record is one row
+    $pdf->Cell($width_cell[0], 12, ($entityOutput[$i]->getEntityId()), 0, 0, 'C', $fill);
+    $pdf->Cell($width_cell[1], 12, ($entityOutput[$i]->getName()), 0, 0, 'L', $fill);
+    $pdf->Cell($width_cell[2], 12, ($entityOutput[$i]->getSalience()), 0, 0, 'C', $fill);
+    $pdf->MultiCell($width_cell[3], 12, ($entityOutput[$i]->getLink()), 0, 1, 'L', !$fill);
+
+    $fill = !$fill;
 }
 
-//Create a PDF table
-/*$header = array('Entity', 'Salience', 'Source');
-$pdf->AddPage();
-$pdf->ImprovedTable($header, $entityOutput);*/
-
-//Return the generated output
+//Return the generated output-------------------------------------------------------------------------------
 $pdf->Output();
 
 ?>
