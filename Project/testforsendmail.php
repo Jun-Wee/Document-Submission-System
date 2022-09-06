304350ce249f5ef3995b737e3be5a3f46101ccbf
AdrianSim2001 <101225244@students.swinburne.edu.my>
2022-09-07 00:47:36 +1000

    fixed testforsendmail.php

--- Project/testforsendmail.php
+++ Project/testforsendmail.php
@@ -8,8 +8,8 @@ $subscriber = $mailtable->getSubscribeConvenor();  //retrieve convenor email
 
 $student_result_by_unit = array();
 
-$j =0;
-for ($i=0; $i < count($subscriber); $i++) {    //2
+$j = 0;
+for ($i = 0; $i < count($subscriber); $i++) {    //2
     $subscriberunit = $mailtable->getConvenorUnit($subscriber[$i]['Email']);
     $mailtable->getAllStudentSubmission();
 
@@ -17,14 +17,13 @@ for ($i=0; $i < count($subscriber); $i++) {    //2
 
     if (!empty($test)) {  //control only convenor that have student submission and result
         $student_result_by_unit[$j] = [  //each convenor's unit table that content student results
-            "name" => $subscriberunit[0]['Name'],  
+            "name" => $subscriberunit[0]['Name'],
             "email" => $subscriberunit[0]['ConvenorEmail'],
             "table" => $mailtable->getFullTable()
-        ];    
+        ];
         $j++;
     }
     $mailtable->unsetFullTable();
-
-    print_r($student_result_by_unit);
     //function to update the isSend mail in submission
 }
+print_r($student_result_by_unit);
