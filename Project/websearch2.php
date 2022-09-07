a9325d7ed22dc73ce42ca238c8b903a55f78f64e
Jun-Wee <tjunwee766@gmail.com>
2022-09-07 22:20:54 +1000

    update error msg

--- Project/websearch2.php
+++ Project/websearch2.php
@@ -1,39 +1,41 @@
 <?php
-//Load Composer's autoloader
-require "src/library/websearch/vendor/autoload.php";
+// composer autoload
+require "vendor/autoload.php";
 
-//include
 require "titlepage.php";
-include "classes/database.php";
-require "classes/webSearchTable.php";
 
-$db = new Database();
-$websearch = new webSearchTable();
 
-$title = $_POST["title"];	
-$title = $websearch->sanitise_input($title);
-$err_msg = "";
+function sanitise_input($data) {
+   		$data = trim($data);
+   		$data = stripslashes($data);
+   		$data = htmlspecialchars($data);
+   		return $data;
+	}
 
-if ($title=="") 
-{
+$title=$_POST["title"];	
+$title=sanitise_input($title);
+
+$err_msg ="";
+
+if ($title=="") {
 	$err_msg .= "<p>Please enter title.</p>";
-}	
-else if (!preg_match("/^[a-zA-Z]$/",$title))
-{
-	$err_msg .= "<p>Title can only contain alpha characters.</p>";
-}	
+	}
+	else if (!preg_match("/^[a-zA-Z]$/",$title)) {
+		$err_msg .= "<p>Title can only contain alpha characters.</p>";
+		}	
 
-if ($err_msg!=""){
-  echo $err_msg;
-}
+    if ($err_msg!=""){
+      echo $err_msg;
+      $err_msg = "";
+    }
 
-//perform search
+
 $queryString = http_build_query([
   'api_key' => '45D285BCD4564CA89DB02392F607A231',
   'engine' => 'google',
   'search_type' => 'scholar',
   'q' => $title,
-  'page' => '1'
+  'page' => 1
 ]);
 
 # make the http GET request
@@ -52,7 +54,7 @@ curl_close($ch);
 # print the JSON response
 $data = json_decode($api_result);
 
-# print the JSON response from SerpWow
+  # print the JSON response from SerpWow
 $data = json_decode($api_result);
 
 $results = $data->scholar_results;
@@ -66,7 +68,11 @@ for( $x=0; $x< 5; $x++)
   #explode function is used to seperate the authors from the rest of the filler content in the displayed link which is seperated by a hyphen
   $splarr = explode("-",$search->displayed_link);
     print_r("Search result: title - ".$search->title.",<br> description - ". $search->snippet .",<br> authors - ".$splarr[0].",<br> link - ". $search->link."<br><br>");
+  
 };
+
+
+
   #explore the below link to understand web search results 
   #https://www.serpwow.com/docs/search-api/results/google/search
 ?>
